d8256fbe9fb3900d8c40c2bc81cfb2f496238576
Bill Dybas <wmdybas@gmail.com>
2018-11-27 21:23:45 -0800

    Setup Backpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
+const PORT = process.env.PORT || 3000
+
 const app = express()
 
 app.use('/', (req, res, next) => {
   res.json({ hello: 'world' })
 })
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
