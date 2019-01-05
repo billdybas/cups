4b5ff246dd65a252c996782213d97c4b1c7a24d1
Bill Dybas <wmdybas@gmail.com>
2019-01-04 20:21:11 -0800

    Create Drinks When Create Cups & Filter By Drink

--- src/api.js
+++ src/api.js
@@ -7,34 +7,125 @@ const db = knex(knexfile[process.env.NODE_ENV || 'development'])
 
 const router = express.Router()
 
-const countCups = async () => {
-  const result = await db.withSchema('app')
-    .from('cups')
-    .count('created_at')
-    .whereRaw(`EXTRACT(year FROM "created_at") = '${new Date().getFullYear()}'`)
+// Accept 'application/json' request bodies
+router.use(express.json())
 
+const countCups = async (name) => {
+  const builder =
+    db.withSchema('app')
+      .from('cups')
+      .count('cups.id')
+      .whereRaw(`EXTRACT(year FROM "cups"."created_at") = '${new Date().getFullYear()}'`)
+
+  if (name) {
+    builder
+      .innerJoin('drinks', 'cups.drink_id', 'drinks.id')
+      .where('drinks.name', name)
+  }
+
+  const result = await builder
   return Number(result[0].count)
+}
+
+const authorize = (req, res, next) => {
+  const secret = req.header('X-Cups-Secret')
+  if (secret !== process.env.CUPS_SECRET) {
+    res.status(401).send('Unauthorized')
+    return
+  }
+  next()
 }
 
 router.get('/cups', async (req, res, next) => {
   const count = await countCups()
 
-  return res.status(200).json({ count })
+  res.status(200).json({ count })
+  return
 })
 
-router.post('/cups.create', async (req, res, next) => {
-  const secret = req.header('X-Cups-Secret')
-  if (secret !== process.env.CUPS_SECRET) {
-    return res.status(401).send('Unauthorized')
+router.get('/cups.filter', async (req, res, next) => {
+  const { name } = req.body
+
+  if (!name) {
+    res.status(400).send('Missing Name')
+    return
   }
 
-  await db.withSchema('app')
-    .from('cups')
-    .insert({})
+  const count = await countCups(name)
 
-  const count = await countCups()
+  res.status(200).json({ count })
+  return
+})
 
-  return res.status(201).json({ success: true, count })
+router.post('/cups.create', authorize, async (req, res, next) => {
+  const { drink } = req.body
+
+  if (!drink) {
+    res.status(400).send('Missing Drink')
+    return
+  }
+
+  db.transaction((trx) =>
+    trx.withSchema('app')
+      .from('drinks')
+      .where('name', drink)
+      .then((rows) => {
+        // Create a drink if one doesn't exist
+        if (rows.length === 0) {
+          return trx
+            .withSchema('app')
+            .insert({ name: drink })
+            .into('drinks')
+            .returning('*')
+        }
+        return rows
+      })
+      .then((rows) => trx // Create a new cup for that drink
+        .withSchema('app')
+        .insert({ drink_id: rows[0].id })
+        .into('cups'))
+  ).then(async () => {
+    const count = await countCups()
+
+    res.status(201).json({ success: true, count })
+    return
+  }).catch((e) => {
+    res.status(500).end()
+  })
+})
+
+router.post('/drinks.create', authorize, async (req, res, next) => {
+  const { drink } = req.body
+
+  if (!drink) {
+    res.status(400).send('Missing Drink')
+    return
+  }
+
+  db.transaction((trx) =>
+    trx.withSchema('app')
+      .from('drinks')
+      .where('name', drink)
+      .then((rows) => {
+        // Create a drink if one doesn't exist
+        if (rows.length === 0) {
+          return trx
+            .withSchema('app')
+            .insert({ name: drink })
+            .into('drinks')
+            .returning('*')
+        }
+        return rows
+      })
+      .then(rows => rows[0])
+  ).then(async (row) => {
+    delete row.id
+
+    res.status(201).json({ success: true, drink: row })
+    return
+  }).catch((e) => {
+    res.status(500).end()
+  })
 })
 
 router.get('/*', (req, res, next) => {
